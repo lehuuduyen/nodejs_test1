853f119e5e922bf5325a06c47f5818565a540f7c
lehuuduyen96 <huuduyenle1996@gmail.com>
2019-07-23 16:25:48 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const authRoute = require('./authRoute');
 const characterRoute = require('./characterRoute');
 const router = express.Router();
-const {ensureAuthenticated} = require('../middleware/authMiddleware');
+const { ensureAuthenticated } = require('../middleware/authMiddleware');
 
-router.use('/',authRoute);
+router.use('/', authRoute);
 router.use('/characters', ensureAuthenticated, characterRoute);
 router.get('/', (req, res) => {
-  return res.render('home', {title: 'Home'})
+    return res.render('home', { title: 'Home' })
 });
 
 module.exports = router;
